d13543a731880238d666c1f0c64419a30201ce5c
Spiritus93 <88580641+Spiritus93@users.noreply.github.com>
2026-08-12 12:33:08 +0200

    Added basic Footer structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './styles/App.scss'
 import Header from "./components/Header/Header.jsx";
 import Home from "./pages/Home/Home.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
       <section id="center">
           <Header />
           <Home />
+          <Footer />
       </section>
   );
 }
