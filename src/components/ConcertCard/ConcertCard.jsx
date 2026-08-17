c258578d759f6a063a9746102517dc8254ab566a
Spiritus93 <88580641+Spiritus93@users.noreply.github.com>
2026-08-17 12:29:01 +0200

    Added a local format for date

--- src/components/ConcertCard/ConcertCard.jsx
+++ src/components/ConcertCard/ConcertCard.jsx
@@ -1,11 +1,13 @@
 import "./ConcertCard.scss";
 
 const ConcertCard = ({ artist, tour, date, location, ticketsSold, capacity }) => {
+    const formattedDate = new Date(date).toLocaleDateString("sl-SI");
+
     return (
         <article className="concert-card">
             <h3>{artist}</h3>
             <p>{tour}</p>
-            <p>{date}</p>
+            <p>{formattedDate}</p>
             <p>{location}</p>
             <p>{ticketsSold} / {capacity} tickets sold</p>
         </article>
